8dc3f2a185b3833bbdc3598dcaef2ef0bc4c229f
Chris Natividad <cenatividad91@gmail.com>
2019-02-27 16:25:46 -0500

    Added comment to test github->jenkins webhook

--- src/main/java/com/revature/Application.java
+++ src/main/java/com/revature/Application.java
@@ -3,6 +3,7 @@ package com.revature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Class to launch Spring Boot
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
