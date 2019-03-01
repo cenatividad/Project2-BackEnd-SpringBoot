5147f01b19d299dc6a25552f3b86e59f2f1e275c
BjornPrime <bjorn291@gmail.com>
2019-03-01 14:15:26 -0500

    project creation works

--- src/main/java/com/revature/controller/ProjectController.java
+++ src/main/java/com/revature/controller/ProjectController.java
@@ -1,6 +1,7 @@
 package com.revature.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import com.revature.service.ProjectService;
 
 @RestController
 @RequestMapping("/project")
+@CrossOrigin(origins="*", allowedHeaders="*")
 public class ProjectController {
 
 		ProjectService projectService;
