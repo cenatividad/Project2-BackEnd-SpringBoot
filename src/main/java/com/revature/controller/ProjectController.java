5e2a95156953b953e622101559d48e558e7f3a4a
Chris Natividad <cenatividad91@gmail.com>
2019-03-02 16:59:38 -0500

    Added endpoint for retrieving stories related to a project.
    GET: /project/{id}/stories

--- src/main/java/com/revature/controller/ProjectController.java
+++ src/main/java/com/revature/controller/ProjectController.java
@@ -1,14 +1,18 @@
 package com.revature.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.models.Project;
+import com.revature.models.Story;
 import com.revature.service.ProjectService;
 
 @RestController
@@ -32,5 +36,10 @@ public class ProjectController {
 		@PostMapping("/viewProject")
 		public Project viewProject(@RequestBody int id) {
 			return this.projectService.viewProject(id);
+		}
+		
+		@GetMapping("/{id}/stories")
+		public List<Story> getStoriesByProject(@PathVariable(name="id") int id){
+			return this.projectService.getStoriesByProject(id);
 		}
 }
