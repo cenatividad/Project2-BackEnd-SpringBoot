2a3007cf7a205eea383e2417a5d782f71a3e6e7b
NoahThacker31 <31545990+NoahThacker31@users.noreply.github.com>
2019-03-04 06:29:03 -0500

    Check Invite Already Exists
    Implemented an if() statement that checks if the invite doesn't exist, or the invite is already set to DECLINED.  If so -> it will be inserted or updated appropriately.  If not, it will return null without any further steps.  This is done to prevent an additional invitation to the same project, or an update from accepted to pending for an already existing invite.

--- src/main/java/com/revature/dtos/InvitationDTO.java
+++ src/main/java/com/revature/dtos/InvitationDTO.java
@@ -16,14 +16,43 @@ public class InvitationDTO {
 	public void setProjectID(int projectID) {
 		this.projectID = projectID;
 	}
-	public InvitationDTO() {
-		super();
-		// TODO Auto-generated constructor stub
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + projectID;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		InvitationDTO other = (InvitationDTO) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (projectID != other.projectID)
+			return false;
+		return true;
 	}
 	@Override
 	public String toString() {
 		return "InvitationDTO [email=" + email + ", projectID=" + projectID + "]";
 	}
-	
-	
+	public InvitationDTO(String email, int projectID) {
+		super();
+		this.email = email;
+		this.projectID = projectID;
+	}
+	public InvitationDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 }
