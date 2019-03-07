bfa9dd4fa17e7bf2c98607046bbbd89ec003f1d2
Chris Natividad <cenatividad91@gmail.com>
2019-03-07 11:56:57 -0500

    Added GET to user/login route to allow login via JWT.
    GET: /user/login
    
    JWT Aspect added around UserService.getLoggedUser() in order to
    extract the user's id from the validated JWT. If JWT validation
    fails, then the JWT Aspect will throw an HttpClientErrorException
    indicating unauthorized access (401).

--- src/main/java/com/revature/controller/UserController.java
+++ src/main/java/com/revature/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 
@@ -20,18 +21,23 @@ import com.revature.models.User;
 import com.revature.service.UserService;
 
 /**
- * Controller for user-oriented requests, delegates requests to necessary services.
+ * Controller for user-oriented requests, delegates requests to necessary
+ * services.
+ * 
  * @author tiand
  */
 @RestController
 @RequestMapping("/users")
-@CrossOrigin(origins="*", allowedHeaders= "*")
+@CrossOrigin(origins = { "http://localhost:4200", "http://s3-us-west-1.amazonaws.com" }, allowedHeaders = {
+		"*" }, methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
+				RequestMethod.OPTIONS }, allowCredentials = "true")
 public class UserController {
 
 	UserService userService;
-	
+
 	/**
 	 * Constructor for the user Controller, initializes the needed services.
+	 * 
 	 * @param userService
 	 */
 	@Autowired
@@ -44,25 +50,26 @@ public class UserController {
 	 * Handles a request to get a user based on its ID.
 	 */
 	@GetMapping("{id}")
-	public User getUserById(@PathVariable(name="id") int id) {
+	public User getUserById(@PathVariable(name = "id") int id) {
 		User user = userService.getUserById(id);
 		if (user == null) {
 			throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
 		}
 		return user;
 	}
-	
+
 	/**
 	 * Handles a request to get all objects related to a specific user.
 	 */
 	@GetMapping("{id}/projects")
-	public List<Project> getUserProjects(@PathVariable(name="id") int id) {
+	public List<Project> getUserProjects(@PathVariable(name = "id") int id) {
 		List<Project> projectList = userService.getUserProjects(id);
 		return projectList;
 	}
-	
+
 	/**
 	 * Handles post requests to create and save a user when signing a new user up.
+	 * 
 	 * @param user
 	 * @return
 	 */
@@ -70,9 +77,15 @@ public class UserController {
 	public User saveUser(@RequestBody User user) {
 		return this.userService.createUser(user);
 	}
+
+	@GetMapping("/login")
+	public User checkLogin() {
+		return this.userService.getLoggedUser(null);
+	}
 	
 	/**
 	 * Handles post requests to log a user in.
+	 * 
 	 * @param user
 	 * @return
 	 */
@@ -80,14 +93,15 @@ public class UserController {
 	public User loginUser(@RequestBody User user) {
 		return this.userService.loginUser(user);
 	}
-	
+
 	/**
 	 * Exception handler that provides meaningful exception messages to the client.
+	 * 
 	 * @param e
 	 * @return
 	 */
 	@ExceptionHandler
-	public ResponseEntity<String> handleException(HttpClientErrorException e){
+	public ResponseEntity<String> handleException(HttpClientErrorException e) {
 		return ResponseEntity.status(e.getStatusCode().value()).body(e.getMessage());
 	}
 }
